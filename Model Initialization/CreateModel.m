eb35f0e688b05d4ddf4d127011444e25a2fb5191
ChristianDahdah <christiandahdah@gmail.com>
2021-02-02 20:55:49 +0100

    [Uploading Initialization folder]

--- Model Initialization/CreateModel.m
+++ Model Initialization/CreateModel.m
@@ -1,5 +1,35 @@
 % Loading Pirat generated matrices
-load("fullmatrices")
+load("FullMatrices");
+
+%% Chaser dimensions
+% Mass and Dimensions for a homogeneous parallelepiped satellite
+
+mC = 1000; % Kg
+
+x = 2; y = 1; z = 1; % Meters
+
+ICDC = (mC/12)* [ y^2+z^2   0   0;
+                 0  x^2+z^2  0
+                 0   0  x^2+y^2 ]; % en kg.m^2
+
+ICDC11 = ICDC(1,1); ICDC12 = ICDC(1,2); ICDC13 = ICDC(1,3);
+ICDC21 = ICDC(2,1); ICDC22 = ICDC(2,2); ICDC23 = ICDC(2,3);
+ICDC31 = ICDC(3,1); ICDC32 = ICDC(3,2); ICDC33 = ICDC(3,3);
+
+%% Target dimensions
+% Mass and Dimensions for a homogeneous parallelepiped satellite
+
+mT = 7000; % Kg
+
+x = 4; y = 1.5; z = 1; % Meters
+
+ITDT = (mT/12)* [ y^2+z^2   0   0;
+                 0  x^2+z^2  0
+                 0   0  x^2+y^2 ]; % en kg.m^2
+
+ITDT11 = ITDT(1,1); ITDT12 = ITDT(1,2); ITDT13 = ITDT(1,3);
+ITDT21 = ITDT(2,1); ITDT22 = ITDT(2,2); ITDT23 = ITDT(2,3);
+ITDT31 = ITDT(3,1); ITDT32 = ITDT(3,2); ITDT33 = ITDT(3,3);
 
 %% Initial conditions (_i subscript)
 alphaDCDT_i = 0.2; betaDCDT_i = 0.1; gammaDCDT_i = 0; % DC -> DT initial Euler angles
@@ -22,19 +52,23 @@ mu= 398600.4418e9; % en m^3/s^2
 rT= 400e3 + 6371e3; % en m 
 w0= sqrt(mu/rT^3);
 
-mC = 12 ; % chaser mass in kg
-
 % State initial values (ce have to give them the same names as in Pirat's
 % symbolic implementation)
-sxDT = sxDT_i; syDT = syDT_i; szDT =  szDT_i; % Initial chaser position wrt target (in target docking frame)
-alphaDCDT = alphaDCDT_i; betaDCDT = betaDCDT_i; gammaDCDT = gammaDCDT_i; % DC -> DT initial Euler angles
 
+% Initial chaser position wrt target (in target docking frame)
+sxDT = sxDT_i; syDT = syDT_i; szDT =  szDT_i;
+
+% DC -> DT initial Euler angles
+alphaDCDT = alphaDCDT_i; betaDCDT = betaDCDT_i; gammaDCDT = gammaDCDT_i;
+
+% Position of the docking port of the chaser in the chaser's frame
 rxDCDC = .1; ryDCDC = .1; rzDCDC = .1;
-rxDTDT = .1; ryDTDT = .1; rzDTDT = .1;
-aDT0 = 50*pi/180; bDT0 = 50*pi/180; cDT0 = 50*pi/180; % Docking port orientation
 
-ICDC11 = 2; ICDC22 = 4; ICDC33 = 6; % en kg.m^2 (refine these values)
-ITDT11 = 2; ITDT22 = 4; ITDT33 = 6;
+% Position of the docking port of the target in the target's frame
+rxDTDT = -.1; ryDTDT = .1; rzDTDT = .1;
+
+% Docking port orientation
+aDT0 = 50*pi/180; bDT0 = 50*pi/180; cDT0 = 50*pi/180;
 
 A  = eval(Ar);
 B = eval(Br);
